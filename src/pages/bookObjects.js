deebdba2afb229aa135da015aba68577141df711
BMoClay <benmorgancleveland@gmail.com>
2024-03-16 16:05:20 -0400

    add BookObjects.js

---
+++ src/pages/bookObjects.js
@@ -0,0 +1,24 @@
+import logo1 from "../assets/logos/Fiery_Color_008 copy 2.png"
+import coverImage1 from '../assets/coverImages/IMG_9389 copy.jpg'
+
+
+export const BOOKS = [
+    {
+        id: 1,
+        bookTitle: "LAWS, MATTER",
+        artist: "Isabelle Cornaro",
+        year: 2024,
+        pages: 44,
+        size: "24 x 30 cm",
+        binding: "staplebound",
+        price: "EU 30.-",
+        edition: "First",
+        editionAmount: 220,
+        attribution: "Published on occasion of the exhibition MOTHER, LAWS, MATTER, from March 21 to June 29, 2024, at Fondazione Giuliani, Roma by e.bs editionsbierisusanne CH-3012 Bern (main office) and IT-06031 Bevagna (PG) (additional office/non-profi t project space Via Santa Maria 7) 2024. Realized by La Tipografica Bevagna.",
+        notes: "No text or explanation, so-called BAT - reproduced and overworked reproductions - combine to form a cinematic outline through spray paintings and their multiple enlargements into infinity.",
+        bio: "Isabelle CORNARO - born 1974, France. Lives and works in Paris and Geneva - her practice includes video, installation, sculpture, painting, scenography as well as edition and artist's books...; she explores the relationship between the object and its image, the original and its copy and the interface between art and art history.",
+        artistsWebsite: "https://isabellecornaro.com/",
+        logo: logo1,
+        coverImage: coverImage1,
+    }
+]
